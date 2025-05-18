71589b743c619e26d9633fc2f1ed46e1747876b0
jackfood <chunghong2004@hotmail.com>
2025-05-18 15:49:52 +0800

    Add files via upload
    Fixed Encoding issue.

--- run.py
+++ run.py
@@ -50,7 +50,7 @@ class SubtitleApp:
 
     def __init__(self, master):
         self.master = master
-        master.title("Auto Subtitle GUI (Portable FFmpeg)")
+        master.title("Auto Subtitle GUI (Portable) V1.0.6")
         master.geometry("700x870")
 
         self.video_files = []
